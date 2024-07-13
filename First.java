b533854136bacd10c2a2e4149143fbd68ce8b0d5
WasimKhan063 <wk8617036@gmail.com>
2024-07-13 04:31:06 +0000

    Try to change code on web(codespace)

--- First.java
+++ First.java
@@ -1,6 +1,8 @@
 public class First{
     public static void main(String[] args) {
         System.out.println("I am learning Git ");
+        int a=10, b=20, c=a+b;
+        System.out.println(c); 
     }
 }
  
